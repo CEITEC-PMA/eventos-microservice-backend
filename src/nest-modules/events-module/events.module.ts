98d6140349d3bc90b2debd915a25937b24f4d318
Adriano Barbosa <adriano.barbosa100@gmail.com>
2023-11-28 21:02:13 +0000

    feat: add event module

--- src/nest-modules/events-module/events.module.ts
+++ src/nest-modules/events-module/events.module.ts
@@ -1,19 +1,15 @@
 import { EventModel } from '@core/event/infra/db/sequelize/eventModel';
-import { EventSequelizeRepository } from '@core/event/infra/db/sequelize/eventSequelizeRepository';
 import { Module } from '@nestjs/common';
-import { SequelizeModule, getModelToken } from '@nestjs/sequelize';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { EventsController } from './events.controller';
+import { EVENT_PROVIDERS } from './events.providers';
 
 @Module({
   imports: [SequelizeModule.forFeature([EventModel])],
   controllers: [EventsController],
   providers: [
-    {
-      provide: EventSequelizeRepository,
-      useFactory: (eventModel: typeof EventModel) =>
-        new EventSequelizeRepository(eventModel),
-      inject: [getModelToken(EventModel)],
-    },
+    ...Object.values(EVENT_PROVIDERS.REPOSITORIES),
+    ...Object.values(EVENT_PROVIDERS.USE_CASES),
   ],
 })
 export class EventsModule {}
