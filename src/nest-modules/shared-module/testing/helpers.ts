2ae6fed485968989a5cff8f9cc5cd732da06cb2a
Adriano Barbosa <adriano.barbosa100@gmail.com>
2023-12-01 14:16:04 +0000

    feat: add helpers

---
+++ src/nest-modules/shared-module/testing/helpers.ts
@@ -0,0 +1,43 @@
+// import { UnitOfWorkSequelize } from '../../../core/shared/infra/db/sequelize/unit-of-work-sequelize';
+
+import { INestApplication } from '@nestjs/common';
+import { getConnectionToken } from '@nestjs/sequelize';
+import { Test, TestingModule } from '@nestjs/testing';
+import { Sequelize } from 'sequelize-typescript';
+import { AppModule } from '../../../app.module';
+import { applyGlobalConfig } from '../../global-config';
+
+export function startApp() {
+  let _app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .overrideProvider('UnitOfWork')
+      .useFactory({
+        factory: (sequelize: Sequelize) => {
+          return new UnitOfWorkSequelize(sequelize as any);
+        },
+        inject: [getConnectionToken()],
+      })
+      .compile();
+    const sequelize = moduleFixture.get<Sequelize>(getConnectionToken());
+
+    await sequelize.sync({ force: true });
+
+    _app = moduleFixture.createNestApplication();
+    applyGlobalConfig(_app);
+    await _app.init();
+  });
+
+  afterEach(async () => {
+    await _app?.close();
+  });
+
+  return {
+    get app() {
+      return _app;
+    },
+  };
+}
