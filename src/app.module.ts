365eb223f8c5e4e716932226d2d384fdd608d06b
Adriano Barbosa <adriano.barbosa100@gmail.com>
2023-12-01 18:16:45 +0000

    feat: add migrate module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './nest-modules/config-module/config.module';
 import { DatabaseModule } from './nest-modules/database-module/database.module';
+import { MigrationsModule } from './nest-modules/database-module/migrations.module';
 import { EventsModule } from './nest-modules/events-module/events.module';
 import { SharedModule } from './nest-modules/shared-module/shared.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule, EventsModule, SharedModule],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    EventsModule,
+    SharedModule,
+    MigrationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
